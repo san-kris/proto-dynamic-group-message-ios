1fc724461e3f955cf8a7e86c71c489f1611ad288
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-22 08:31:32 -0500

    Initial Commit

---
+++ proto-dynamic-group-message-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto-dynamic-group-message-ios
+//
+//  Created by Santosh Krishnamurthy on 3/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
